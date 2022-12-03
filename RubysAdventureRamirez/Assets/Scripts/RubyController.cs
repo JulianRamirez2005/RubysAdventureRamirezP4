42fb0d1ad129ff54998e8a9d6440b457a2cf8b2e
JulianRamirez2005 <solokiller2005@gmail.com>
2022-12-03 12:44:32 -0800

    I give up
    did a whole lot of work

---
+++ RubysAdventureRamirez/Assets/Scripts/RubyController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+  
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //create 2 variables to use the unity builtt in axes
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        
+    
+    //create my movement vector cariable
+    Vector2 position = rigidbody2d.position;
+
+        //create the movement of my character
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        //Set the new position
+        rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    
+}
