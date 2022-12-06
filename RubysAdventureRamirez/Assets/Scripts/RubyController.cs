0fb28efed199ebc0c227ce31e02eb811b5abb9af
JulianRamirez2005 <solokiller2005@gmail.com>
2022-12-06 11:44:33 -0800

    damage
    damage done

--- RubysAdventureRamirez/Assets/Scripts/RubyController.cs
+++ RubysAdventureRamirez/Assets/Scripts/RubyController.cs
@@ -6,7 +6,7 @@ public class RubyController : MonoBehaviour
 {
     public int maxHealth = 5;
     public int health { get { return currentHealth; } }
-    int currentHealth;
+    public int currentHealth;
 
   
 
@@ -14,11 +14,14 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
     public float speed = 3.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
@@ -44,6 +47,15 @@ public class RubyController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
